8bc995776ac608c0d78ade8b6749b05c11e81e83
Spencer Alderman <spencer@1rogue.com>
2015-03-03 21:27:14 -0500

    Forgot the clause of the error, the parent dependency only needs a static interface method

--- src/main/java/com/codelanx/bugexample/BugExample.java
+++ src/main/java/com/codelanx/bugexample/BugExample.java
@@ -1,26 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.codelanx.bugexample;
 
-import com.codelanx.codelanxlib.command.TabInfo;
-import java.util.ArrayList;
-
-
-/**
- * Class description for {@link BugExample}
- *
- * @since 1.0.0
- * @author 1Rogue
- * @version 1.0.0
- */
-public class BugExample {
-
-    public void foo() {
-        TabInfo info = new TabInfo();
-        info.map(0, () -> new ArrayList<>());
-    }
-
-}
+public class BugExample {}
